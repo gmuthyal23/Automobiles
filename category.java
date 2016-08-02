1c565f4cce58cd112ebaf2b5454f3736ce18145c
gmuthyal23 <gopinathmuthyala@gmail.com>
2016-08-02 14:10:43 +0530

    Add files via upload

---
+++ category.java
@@ -0,0 +1,10 @@
+package com.test.home;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class category {
+int id;
+String name;
+String address;
+}
---
+++ testcategory.java
@@ -0,0 +1,17 @@
+package com.test.home;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class testcategory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext();
+		context.scan("com.test");
+		context.refresh();
+		context.getBean("category");
+		System.out.println("bean created Successfully:");
+
+	}
+
+}
