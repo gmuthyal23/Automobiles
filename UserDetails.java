9d28803b5e280c2880935777a503f85a5ccf862c
gmuthyal23 <gopinathmuthyala@gmail.com>
2016-09-08 12:42:15 +0530

    Add files via upload

---
+++ UserDetails.java
@@ -0,0 +1,61 @@
+package Com.Automobiles.Shopping.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table
+@Component
+public class UserDetails {
+	@Id
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String id;
+	private String name;
+	private String password;
+	private String mail;
+	private String contact;
+	private String address;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
